4d6f6d332d3b079c217f1fd40b86fde3bb88e1d9
Samuel Peter <96541225+samuelpeter17@users.noreply.github.com>
2023-09-08 23:19:21 -0700

    Update Problem5.py

--- Problem5.py
+++ Problem5.py
@@ -3,10 +3,11 @@ import timeit
 #Smallest multiple
 def calc():
     # Number should be a multiple of all the prime factors
-    num = 2*3*5*7*11*13*17*19
+    num = 2*3*5*7*11*13*17*19 + 10  #Add 10 to make the second last digit even. Last digit is already zero. Last two digits must be an even number and zero for num to be divisible by 20 and 10.
     factors = list(range(1,21))     #list of factors
     k = 0
     while (k == 0): #While the flag stays down
+        print(num)
         #Go through the list of factors
         for i in factors:
             x = num % i         #Calculate the remainder from division
@@ -25,55 +26,16 @@ def calc():
         
         #If out of for loop and flag is down, increment num.
         if k == 0:
-            num = num + 2    # Number is even since even numbers can't be factors of odd numbers, but odd numbers can be factors of both odd and even numbers.
+            num = num + 20    # Number is even since even numbers can't be factors of odd numbers, but odd numbers can be factors of both odd and even numbers.
+            #We add 20 because the final number must end in a multiple of 20.
         else:       #Else, the smallest multiple of 1-20 is found.
             break
     print(num, " is the answer.")
             
 #Function call
 startTime = timeit.default_timer()
-print(startTime)
 calc()
 endTime = timeit.default_timer()
 
 runTime = (endTime - startTime)
-print("Runtime : ", runTime, " s")        # Fastest Runtime: import timeit
-
-#Smallest multiple
-def calc():
-    # Number should be a multiple of all the prime factors
-    num = 2*3*5*7*11*13*17*19
-    factors = list(range(1,21))     #list of factors
-    k = 0
-    while (k == 0): #While the flag stays down
-        #Go through the list of factors
-        for i in factors:
-            x = num % i         #Calculate the remainder from division
-            if i == 20:         #If the factor is 20 
-                #With no remainder, raise the flag.
-                if x == 0:
-                    k = 1
-                    break
-                else:           #Else, keep it down and break to the next number.
-                    k = 0
-                    break
-            else:               #If there is a remainder with any other factor, break and move onto the next number.
-                if x != 0:
-                    k = 0
-                    break
-        
-        #If out of for loop and flag is down, increment num.
-        if k == 0:
-            num = num + 2    # Number is even since even numbers can't be factors of odd numbers, but odd numbers can be factors of both odd and even numbers.
-        else:       #Else, the smallest multiple of 1-20 is found.
-            break
-    print(num, " is the answer.")
-            
-#Function call
-startTime = timeit.default_timer()
-print(startTime)
-calc()
-endTime = timeit.default_timer()
-
-runTime = (endTime - startTime)
-print("Runtime : ", runTime, " s")        # Fastest Runtime: 37.94184410001617  sec
+print("Runtime : ", runTime, " s")        # Fastest Runtime: 37.94184410001617  s
